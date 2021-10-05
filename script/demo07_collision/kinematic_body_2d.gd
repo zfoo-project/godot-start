0aff1567f3d153e6aefe0b2128fe4e07f285153a
jaysunxiao <jaysunxiao.gmail.com>
2021-10-05 13:50:57 +0800

    doc

---
+++ script/demo07_collision/kinematic_body_2d.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+
+# How fast the player will move (pixels/sec).
+export var speed = 400
+# Size of the game window.
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+
+	if velocity.length() <= 0:
+		return
+	
+	velocity = velocity.normalized() * speed
+	
+	var info: KinematicCollision2D = move_and_collide(velocity * delta)
+	if info != null:
+		print(info.collider.name)	
