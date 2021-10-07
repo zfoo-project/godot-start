8b46584bb591bfb8eef829c39c57a1cbbbdeab9e
jaysunxiao <jaysunxiao.gmail.com>
2021-10-07 14:13:18 +0800

    doc

--- zfoo/util/NodeUtils.gd
+++ zfoo/util/NodeUtils.gd
@@ -1,5 +1,6 @@
 extends Object
 
+const StringUtils = preload("res://zfoo/util/StringUtils.gd")
 
 # 移除node节点
 static func removeNode(node: Node) -> void:
@@ -19,3 +20,17 @@ static func addScene(node: Node, scene: PackedScene) -> Node:
 	var sceneNode = scene.instance()
 	node.add_child(sceneNode)
 	return sceneNode
+
+# 转换场景
+static func changeScene(sceneTree: SceneTree, scenePath: String) -> void:
+	var error = sceneTree.change_scene(scenePath)
+	if error == OK:
+		pass
+	printerr(StringUtils.format("SceneTree[{}]加载场景[scenePath:{}]异常[{}]", [sceneTree, scenePath, error]))
+
+	
+static func changeSceneTo(sceneTree: SceneTree, scene: PackedScene) -> void:
+	var error = sceneTree.change_scene_to(scene)
+	if error == OK:
+		pass
+	printerr(StringUtils.format("SceneTree[{}]加载场景[scene:{}]异常[{}]", [sceneTree, scene, error]))
