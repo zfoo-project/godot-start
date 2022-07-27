f6430ceae99fa0e7ea84da7f6ec941cc73fa8c26
godotg <jaysunxiao.gmail.com>
2022-07-27 16:02:18 +0800

    doc

--- script/demo16_http/http_request.gd
+++ script/demo16_http/http_request.gd
@@ -3,14 +3,14 @@ extends HTTPRequest
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	httpRequest()
 	pass
 
 func httpRequest():
 	var headers: PoolStringArray = []
 	self.set_use_threads(true)
 	self.connect("request_completed", self, "doSomething")
-	#self.request("https://swapi.dev/api/people/1/", headers, true, HTTPClient.METHOD_PUT, "name: Jane Skywalker")
-	self.request("https://www.taptap.com/")
+	self.request("https://www.baidu.com/")
 		
 
 func doSomething(result, response_code, headers, body: PoolByteArray):
